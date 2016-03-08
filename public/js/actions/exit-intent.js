9d0ff5961c13cba7377067f0c96273c79b038336
Webdesignwill <will@webdesignwill.co.uk>
2016-03-08 17:03:24 +0000

    example js and add closure

--- public/js/actions/exit-intent.js
+++ public/js/actions/exit-intent.js
@@ -1,41 +1,44 @@
+(function () {
 
-"use strict";
+  "use strict";
 
-var config = {},
-  sensitivity = 20,
-  timer = 1000,
-  delay = 0,
-  _delayTimer = null,
-  _html = document.documentElement;
+  var config = {},
+    sensitivity = 20,
+    timer = 1000,
+    delay = 0,
+    _delayTimer = null,
+    _html = document.documentElement;
 
-function setDefaultCookieExpire() {
-  var ms = 14*24*60*60*1000;
+  function setDefaultCookieExpire() {
+    var ms = 14*24*60*60*1000;
 
-  var date = new Date();
-  date.setTime(date.getTime() + ms);
+    var date = new Date();
+    date.setTime(date.getTime() + ms);
 
-  return "; expires=" + date.toUTCString();
-}
-
-function fire() {
-  console.log('User is trying to leave');
-}
-
-function handleMouseleave(e) {
-  if (e.clientY > sensitivity) { return; }
-  _delayTimer = setTimeout(fire, delay);
-}
-
-function handleMouseenter() {
-  if (_delayTimer) {
-    clearTimeout(_delayTimer);
-    _delayTimer = null;
+    return "; expires=" + date.toUTCString();
   }
-}
 
-function appendPopup() {
-  _html.addEventListener('mouseleave', handleMouseleave);
-  _html.addEventListener('mouseenter', handleMouseenter);
-}
+  function fire() {
+    console.log('User is trying to leave');
+  }
 
-setTimeout(appendPopup, timer);
+  function handleMouseleave(e) {
+    if (e.clientY > sensitivity) { return; }
+    _delayTimer = setTimeout(fire, delay);
+  }
+
+  function handleMouseenter() {
+    if (_delayTimer) {
+      clearTimeout(_delayTimer);
+      _delayTimer = null;
+    }
+  }
+
+  function appendPopup() {
+    _html.addEventListener('mouseleave', handleMouseleave);
+    _html.addEventListener('mouseenter', handleMouseenter);
+  }
+
+  setTimeout(appendPopup, timer);
+
+})();
